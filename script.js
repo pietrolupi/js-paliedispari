5f4a94ec219a650a1e3fab33e0ac6cd25646e147
pietrolupi <pietro.lupi92@gmail.com>
2023-09-15 16:13:20 +0200

    [edit]:js palindromo fatto

--- script.js
+++ script.js
@@ -11,24 +11,34 @@
 
 
 const parolaIns = prompt('inserisci una parola');
-let reverseArray = []
-
-
+let firstHalfArray = []
+let messaggio = document.getElementById('messaggio');
+let flag = false;
 function palindrome(parola){
 
   let letterArray = parola.split('');
   ; //qui va la seconda parte di parola
 
-  for( let i = 0; i < letterArray.length / 2; i++){
-
-    reverseArray.push(letterArray[i]);
+  for( let i = 0; i < Math.floor(letterArray.length / 2); i++){
+    firstHalfArray.push(letterArray[i]); //metto la prima metà della parola in una nuova lista
   }
-  
 
+  //DOPO il ciclo RIMUOVO la prima metà delle lettere nella lista, restando con l'ultima metà. 
+  letterArray.splice(0, Math.ceil(letterArray.length / 2) );
+
+  if(letterArray.reverse().join('') === firstHalfArray.join('')){
+    messaggio.innerHTML = 'BRAVONE è proprio un palindromo!'
+  }else{
+    messaggio.innerHTML = 'OH NO, non è un palindromo :('
+  };
+
+  console.log(firstHalfArray)
+  console.log(letterArray.reverse())
+  console.log(flag);
 }
 
 
 palindrome(parolaIns);
 
-console.log(reverseArray)
 
+console.log(letterArray)
